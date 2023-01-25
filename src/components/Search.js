084e33cc5d33d61df44d3223b4f7c5c65dfcf0ac
Isaque <zazac3179@gmail.com>
2023-01-24 21:22:07 -0300

    req 3

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,14 +2,55 @@ import React, { useContext } from 'react';
 import PlanetsContext from '../context/PlanetsContext';
 
 function Search() {
-  const { handleChange } = useContext(PlanetsContext);
+  const {
+    number,
+    handleChange,
+    handleClick,
+    handleChangeColum,
+    handleChangeComparasion,
+    handleChangeNumber,
+  } = useContext(PlanetsContext);
 
   return (
-    <input
-      type="text"
-      data-testid="name-filter"
-      onChange={ ({ target: { value } }) => handleChange(value) }
-    />
+    <form>
+      <input
+        type="text"
+        data-testid="name-filter"
+        onChange={ ({ target: { value } }) => handleChange(value) }
+      />
+      <select
+        data-testid="column-filter"
+        onChange={ ({ target: { value } }) => handleChangeColum(value) }
+      >
+        <option value="population">population</option>
+        <option value="orbital_period">orbital_period</option>
+        <option value="diameter">diameter</option>
+        <option value="rotation_period">rotation_period</option>
+        <option value="surface_water">surface_water</option>
+      </select>
+      <select
+        data-testid="comparison-filter"
+        onChange={ ({ target: { value } }) => handleChangeComparasion(value) }
+      >
+        <option value="maior que">maior que</option>
+        <option value="menor que">menor que</option>
+        <option value="igual a">igual a</option>
+      </select>
+      <input
+        data-testid="value-filter"
+        type="number"
+        value={ number }
+        onChange={ ({ target: { value } }) => handleChangeNumber(value) }
+      />
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={ handleClick }
+      >
+        Filtrar
+
+      </button>
+    </form>
   );
 }
 
--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -5,7 +5,10 @@ import useFetch from '../hooks/useFetch';
 
 export default function PlanetsProvider({ children }) {
   const [planets, setPlanetsApi] = useState([]);
-  const [planetsFiltered, setFilter] = useState(planets);
+  const [planetsFiltered, setFilter] = useState([]);
+  const [column, setColumn] = useState('population');
+  const [comparison, setComparison] = useState('maior que');
+  const [number, setNumber] = useState(0);
   const { makeFetch } = useFetch();
 
   useEffect(() => {
@@ -21,7 +24,46 @@ export default function PlanetsProvider({ children }) {
     const filterPlanets = planets.filter((p) => p.name.includes(value));
     setFilter(filterPlanets);
   };
-  const values = { planetsFiltered, planets, handleChange };
+
+  const handleChangeComparasion = (value) => {
+    setComparison(value);
+  };
+
+  const handleChangeColum = (value) => {
+    setColumn(value);
+  };
+
+  const handleChangeNumber = (value) => {
+    setNumber(value);
+  };
+
+  const handleClick = () => {
+    const err = 'deu ruim';
+    const filterNumbers = planets.filter((p) => {
+      switch (comparison) {
+      case 'maior que':
+        return Number(p[column]) > Number(number);
+      case 'menor que':
+        return Number(p[column]) < Number(number);
+      case 'igual a':
+        return Number(p[column]) === Number(number);
+      default:
+        return err;
+      }
+    });
+    setFilter(filterNumbers);
+  };
+
+  const values = {
+    planetsFiltered,
+    planets,
+    number,
+    handleChange,
+    handleClick,
+    handleChangeColum,
+    handleChangeComparasion,
+    handleChangeNumber,
+  };
 
   return (
     <PlanetsContext.Provider value={ values }>
