98eae79d60149d370f077cbe9ddb7d85ea4ffbd1
Isaque <zazac3179@gmail.com>
2023-01-26 20:45:21 -0300

    req 4

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,46 +2,58 @@ import React, { useContext } from 'react';
 import PlanetsContext from '../context/PlanetsContext';
 
 function Table() {
-  const { planetsFiltered } = useContext(PlanetsContext);
+  const { planetsFiltered, filterMoment } = useContext(PlanetsContext);
+  console.log(filterMoment);
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Rotation Period</th>
-          <th>Orbital Period</th>
-          <th>Diameter</th>
-          <th>Climate</th>
-          <th>Gravity</th>
-          <th>Terrain</th>
-          <th>Surface Water</th>
-          <th>Population</th>
-          <th>Films</th>
-          <th>Created</th>
-          <th>Edited</th>
-          <th>URL</th>
-        </tr>
-      </thead>
-      <tbody>
-        {planetsFiltered.map((planet) => (
-          <tr key={ planet.name }>
-            <td>{planet.name}</td>
-            <td>{planet.rotation_period}</td>
-            <td>{planet.orbital_period}</td>
-            <td>{planet.diameter}</td>
-            <td>{planet.climate}</td>
-            <td>{planet.gravity}</td>
-            <td>{planet.terrain}</td>
-            <td>{planet.surface_water}</td>
-            <td>{planet.population}</td>
-            <td>{planet.films}</td>
-            <td>{planet.created}</td>
-            <td>{planet.edited}</td>
-            <td>{planet.url}</td>
-          </tr>
+    <>
+      <ul>
+        {filterMoment.map((f, i) => (
+          <li key={ i }>
+            {`${f.column} ${f.comparison} ${f.number}`}
+            {' '}
+            <button>x</button>
+          </li>
         ))}
-      </tbody>
-    </table>
+      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {planetsFiltered.map((planet, i) => (
+            <tr key={ i }>
+              <td>{planet.name}</td>
+              <td>{planet.rotation_period}</td>
+              <td>{planet.orbital_period}</td>
+              <td>{planet.diameter}</td>
+              <td>{planet.climate}</td>
+              <td>{planet.gravity}</td>
+              <td>{planet.terrain}</td>
+              <td>{planet.surface_water}</td>
+              <td>{planet.population}</td>
+              <td>{planet.films}</td>
+              <td>{planet.created}</td>
+              <td>{planet.edited}</td>
+              <td>{planet.url}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
 
--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -5,6 +5,7 @@ import useFetch from '../hooks/useFetch';
 
 export default function PlanetsProvider({ children }) {
   const [planets, setPlanetsApi] = useState([]);
+  const [filterMoment, setFilterMoment] = useState([]);
   const [planetsFiltered, setFilter] = useState([]);
   const [column, setColumn] = useState('population');
   const [comparison, setComparison] = useState('maior que');
@@ -37,27 +38,46 @@ export default function PlanetsProvider({ children }) {
     setNumber(value);
   };
 
+  // const filterTest = { column, comparison, number };
+  // setFilterMoment((filterPrev) => [...filterPrev, filterTest]);
+  // filterMoment.map((inf) => {
+  //   console.log(inf);
+  //   if (inf.comparison === 'maior que') {
+  //     const filter = planets.filter((p) => Number(p[column]) > Number(number));
+  //     setFilter(filter);
+  //   } if (inf.comparison === 'menor que') {
+  //     const filter = planets.filter((p) => Number(p[column]) < Number(number));
+  //     setFilter(filter);
+  //   } if (inf.comparison === 'igual a') {
+  //     const filter = planets.filter((p) => Number(p[column]) === Number(number));
+  //     setFilter(filter);
+  //   }
+  //   return planets;
+  // });
+
   const handleClick = () => {
-    const err = 'deu ruim';
-    const filterNumbers = planets.filter((p) => {
-      switch (comparison) {
-      case 'maior que':
-        return Number(p[column]) > Number(number);
-      case 'menor que':
-        return Number(p[column]) < Number(number);
-      case 'igual a':
-        return Number(p[column]) === Number(number);
-      default:
-        return err;
-      }
-    });
-    setFilter(filterNumbers);
+    const filterTest = { column, comparison, number };
+    setFilterMoment((filterPrev) => [...filterPrev, filterTest]);
+    if (comparison === 'maior que') {
+      setFilter(filterMoment.length === 0
+        ? planets.filter((d) => Number(d[column]) > Number(number))
+        : planetsFiltered.filter((d) => Number(d[column]) > Number(number)));
+    } else if (comparison === 'menor que') {
+      setFilter(filterMoment.length === 0
+        ? planets.filter((d) => Number(d[column]) < Number(number))
+        : planetsFiltered.filter((d) => Number(d[column]) < Number(number)));
+    } else if (comparison === 'igual a') {
+      setFilter(filterMoment.length === 0
+        ? planets.filter((d) => Number(d[column]) === Number(number))
+        : planetsFiltered.filter((d) => Number(d[column]) === Number(number)));
+    }
   };
 
   const values = {
     planetsFiltered,
     planets,
     number,
+    filterMoment,
     handleChange,
     handleClick,
     handleChangeColum,
